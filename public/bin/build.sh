bf22e50c101dfba092f87bece2c2e1901215eb88
Michał Kutrzeba <kutrzeba.michal@gmail.com>
2017-05-27 22:28:58 +0200

    New dir tree

--- public/bin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-basePath=`pwd + "/.."`
-
-bower install ${basePath}
-
-# Building frontend
-mkdir ${basePath}/dist
-../node_modules/babel-cli/bin/babel.js ${basePath}public --out-dir ${basePath}/dist
